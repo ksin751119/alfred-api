9abc11b3b96b4981e9b8f5321b015c3165cbefcf
weizard <purpledoor4921@gmail.com>
2024-03-24 02:42:52 +0800

    fix: bypass cors issue

--- run_script/run_deploy.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-# yarn sls offline -s alpha
-# yarn sls deploy --aws-profile ksin751119 -r ap-northeast-1 -s beta
-# ethtaipei2024
-# https://devv.ai/search?threadId=dgbseqxwbe2o
-
-# serverles deploy --aws-profile profileName
-yarn run deploy:beta
